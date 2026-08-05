fe97f292488c86ddab0f7e70b3f3072005b38b09
Aluno Vocaçao <>
2026-08-05 09:01:15 -0300

    update

---
+++ Modulo01/README_desafio.py
@@ -0,0 +1,26 @@
+# 🥖 Sistema de Vendas CLI - Padaria
+
+'''
+Projeto desenvolvido como parte do **Desafio Prático: Repositório Aberto**.
+Trata-se de uma aplicação em Python executada no terminal (CLI) para gerenciamento de estoque, vendas e cadastro de produtos de uma padaria.
+
+
+
+## 🚀 Funcionalidades
+
+- ** Cadastrar Produto:** Registra nome, descrição, validade, quantidade em estoque e preço.
+- **Listar Produtos:** Exibe todos os itens disponíveis no estoque.
+- **Excluir Produto:** Remove um item cadastrado pelo nome.
+- **Pesquisar Produto:** Consulta rápida de itens no estoque.
+- **Realizar Venda:** Atualiza a quantidade em estoque e calcula o valor total da compra.
+- **Cancelar Venda:** Devolve a quantidade de itens vendidos de volta ao estoque.
+- **Suporte ao Cliente:** Canais de atendimento em terminal.
+
+---
+
+## 🛠️ Tecnologias Utilizadas
+
+- **Linguagem:** Python 3.x
+- **Interface:** CLI (Command Line Interface / Terminal)
+
+'''
